4f8cfdd44b4a46403aed60d897c4f83fff7deace
Dani Guardiola <contacto@daniguardiola.me>
2014-10-06 14:43:01 +0200

    -Gruntfile.js fixed

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
             'style/*.css',
             'libs/*.css'
           ],
-          dest: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.css'
+          dest: 'materializer.min.css'
         },
         js: {
           src: [
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
             'style/*.js',
             'libs/*.js'
           ],
-          dest: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.js'
+          dest: 'materializer.min.js'
         }
       },
       autoprefixer: {
@@ -33,25 +33,20 @@ module.exports = function(grunt) {
           browsers: ["last 2 versions", "ie 9", "iOS 6", "Safari 6.2", "ChromeAndroid 25", "FirefoxAndroid 20", 'opera 12', 'ff 15', 'chrome 25']
         },
         single_file: {
-          src: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.css',
-          dest: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.css'
+          src: 'materializer.min.css',
+          dest: 'materializer.min.css'
         }
       },
       cssmin: {
         minify: {
-          src: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.css',
-          dest: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.min.css'
+          src: 'materializer.min.css',
+          dest: 'materializer.min.css'
         }
       },
       uglify: {
         build: {
-          src: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.js',
-          dest: '/home/daniguardiola/proyectos/daniguardiola.me/materializer/materializer.min.js'
-        }
-      },
-      exec: {
-        upload: {
-          cmd: "lftp -f upload.x"
+          src: 'materializer.min.js',
+          dest: 'materializer.min.js'
         }
       },
       materializer: {
@@ -67,9 +62,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-exec');
 
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['concat',"autoprefixer",'cssmin','uglify',"exec"]);
+  grunt.registerTask('default', ['concat',"autoprefixer",'cssmin','uglify']);
 
 };
