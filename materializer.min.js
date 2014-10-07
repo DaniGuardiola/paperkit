6af19a5121415ce94ef341d79cc9d7ee12dcfd30
Daniel Muñoz Guardiola <dani.guardiola@datatrends.es>
2014-10-07 12:08:49 +0200

    -grunt-materializer updated (not working)

---
+++ materializer.min.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+var properties = {
+  logEnabled: false,
+  log: function(what){
+    if (properties.logEnabled) {
+      console.log(what);
+    }
+  },
+  change: function(query,classOut,classIn){
+    // TODO: Add support for querySelectorAll
+    var element = document.querySelector(query);
+    if (element) {
+      if (classOut) {
+        if (element.classList.contains(classOut)) {
+          element.classList.remove(classOut);
+        }
+      }
+      if (classIn) {
+        if (element.classList.contains(classIn) != true) {
+          element.classList.add(classIn);
+        }
+      }
+      properties.log("properties.change | " + query);
+    } else {
+      properties.log("!! properties.change | query not specified or not valid");
+    }
+  }
+}
