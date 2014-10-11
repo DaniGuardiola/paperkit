1a0112537de5ff3edec3a4574bed182f6ba28fae
David María Arribas <dmariaa@datatrends.es>
2014-10-11 20:25:08 +0200

    - Added parsing of dip units

--- grunt-materializer/tasks/materializer.js
+++ grunt-materializer/tasks/materializer.js
@@ -8,74 +8,19 @@
 
 'use strict';
 
+var Materializer = require("materializer");
+
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('materializer', 'The easy Material Design framework, easier', function() {
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       mdPath: './'
     });
-
-    var objectsMap = {};
-
-    var processMCSS= function(mcssFile) {
-      var mcssData = mcssFile.split('\n');
-
-      for(var i=0; i < mcssData.length; i++) {
-        var prop = mcssData[i].match(/\s*(.*?)\s*:/);
-
-        if(prop != null && objectsMap[prop[1]]) {
-
-          var value = mcssData[i].match(/.*:\s*(.*?)\s*;\s*/);
-
-          var repl = objectsMap[prop[1]][value[1]];
-          if(repl) {
-            mcssData[i] = "  /* " + value[1] + "*/\n  " + repl.replace(/;/g, ';\n  ') + ";\n";
-          }
-        }
-      }
-
-      return mcssData.join('\n');
-    };
-
-    var parseMaterializer = function(text) {
-      var re = /\[(.*?)]\s*{\s*(.*?)\s*}/mg;
-      var result;
-
-      text = text.replace(/\n/g, "");
-
-      do {
-        result = re.exec(text);
-        if(result != null) {
-          if(result[1].search(/\],\**\[/) > -1) {
-            var keys = result[1].split(/\],\**\[/);
-
-            for(var i=0; i < keys.length; i++) {
-              parseSingle(keys[i], result[2]);
-            }
-
-          } else {
-            parseSingle(result[1], result[2]);
-          }
-        }
-      } while(result != null)
-    };
-
-    var parseSingle = function(key, value) {
-      var keys = key.split("=");
-      var obj = {};
-
-      if(objectsMap[keys[0]]) {
-        obj = objectsMap[keys[0]];
-      }
-
-      obj[keys[1]] = value;
-
-      objectsMap[keys[0]] = obj;
-    };
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
@@ -92,7 +37,7 @@ module.exports = function(grunt) {
         return false;
       }
 
-      parseMaterializer(mdFile);
+      var materializer = new Materializer(mdFile);
 
       var src = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
@@ -109,7 +54,7 @@ module.exports = function(grunt) {
       }).join('\n');
 
       // Handle options.
-      src = processMCSS(src);
+      src = materializer.process(src);
 
       // Write the destination file.
       grunt.file.write(f.dest, src);
