f97b4fa66510bc52e0927fcd63cdff1660ce20b7
Dani Guardiola <contacto@daniguardiola.me>
2014-10-16 13:20:25 +0200

    Gruntfile improvements and cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,95 @@
 module.exports = function(grunt) {
 
-  // 1. All configuration goes here
+  // Configuration
   grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      
+
+      // 1 - Cleaning bin and demo
+      clean: [
+        'bin/',
+        'demo/',
+        'grunt-materializer/sources/'
+        ],
+
+      //cssbeautifier:{files:[    "source/**/*.css"    ]},
+
+      // 2 - Compiling json to css on bin/materializer/
       compiler: {
         css: {
           expand: true,
           src: [
-            'source/animation/*.json',
-            'source/components/*.json',
-            'source/layout/*.json',
-            'source/style/*.json',
-            'source/libs/*.json'
+            'source/style/*.json'
           ],
           dest: 'bin/',
           ext: '.css',
-          rename: function(dest, src) {            
+          rename: function(dest, src) {
             var newDest = dest + src.replace("source", "materializer");
-            // console.log("DEST => " + dest + " SRC => " + src + " NEW => " + newDest);
             return newDest;
           }
         }
       },
+
+      // 3 - Coping everything
+      copy: {
+        main: {
+          expand: true,
+          src: [
+            'source/class/*',
+            'source/css-first/*',
+            '!source/**.json'
+          ],
+          dest: 'bin/',
+          rename: function(dest, src) {
+            var newDest = dest + src.replace("source", "materializer");
+            return newDest;
+          }
+        },
+        resources: {
+          expand: true,
+          src: 'source/md-resources/**',
+          dest: 'bin/',
+          rename: function(dest, src) {
+            var newDest = dest + src.replace("source/", "");
+            return newDest;
+          }
+        },
+        jsonToMdcss: {
+          expand: true,
+          src: 'source/**/*.json',
+          dest: 'grunt-materializer/sources/',
+          flatten: true
+        },
+        mdcss: {
+          src: 'grunt-materializer/**',
+          dest: 'bin/'
+        }
+
+      },
+
+      // 4 - Autoprefixing all css from bin/materializer/
+      autoprefixer: {
+        options: {
+          browsers: ["last 2 versions", "ie 9", "iOS 6", "Safari 6.2", "ChromeAndroid 25", "FirefoxAndroid 20", 'opera 12', 'ff 15', 'chrome 25']
+        },
+        prefix: {
+          src: [
+            'bin/materializer/style/*.css',
+            'bin/materializer/class/*.css',
+            'bin/materializer/css-first/*.css'
+          ]
+        }
+      },
+
+      // 4 - Beautify everything
+      cssbeautifier:{files:[    "bin/materializer/**/*.css"    ]},
+
+      // 5 - Concatenating css and js from bin/materializer/ to bin/
       concat: {
-        // 2. Configuration for concatinating files goes here.
         css: {
           src: [
-            'bin/materializer/animation/*.css',
-            'bin/materializer/components/*.css',
-            'bin/materializer/layout/*.css',
+            'bin/materializer/css-first/*.css',
             'bin/materializer/style/*.css',
-            'bin/materializer/libs/*.css'
+            'bin/materializer/class/*.css'
           ],
           dest: 'bin/materializer.css'
         },
@@ -46,39 +104,39 @@ module.exports = function(grunt) {
           dest: 'bin/materializer.js'
         }
       },
-      autoprefixer: {
-        options: {
-          browsers: ["last 2 versions", "ie 9", "iOS 6", "Safari 6.2", "ChromeAndroid 25", "FirefoxAndroid 20", 'opera 12', 'ff 15', 'chrome 25']
-        },
-        single_file: {
-          src: 'bin/materializer.css',
-          dest: 'bin/materializer.css'
-        }
-      },
+
+      // 6 - Minify css
       cssmin: {
         minify: {
           src: 'bin/materializer.css',
           dest: 'bin/materializer.min.css'
         }
       },
+
+      // 7 - Uglify css
       uglify: {
         build: {
           src: 'bin/materializer.js',
           dest: 'bin/materializer.min.js'
         }
-      }      
+      }
   });
 
+
+  // Loading our custom tasks
   grunt.loadTasks('tasks');
   grunt.loadTasks('tasks/grunt-materializer-compiler');
 
-  // 3. Where we tell Grunt we plan to use this plug-in.
+  // Loading NPM tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-cssbeautifier');
 
-  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['compiler','concat',"autoprefixer",'cssmin','uglify']);
+  // This will run when executing grunt
+  grunt.registerTask('default', ['clean','compiler','copy',"autoprefixer",'cssbeautifier','concat','cssmin','uglify']);
 
 };
