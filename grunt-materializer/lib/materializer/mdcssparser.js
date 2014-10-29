d7996a14bd19875be1bc7a0fe0adffae8f4c9f7a
DaniGuardiola <contacto@daniguardiola.me>
2014-10-29 21:07:49 +0100

    - Created tools folder - Added generator of md-color.json

---
+++ grunt-materializer/lib/materializer/mdcssparser.js
@@ -0,0 +1,93 @@
+fs = require('fs')
+winston = require('winston')
+css = require('css')
+sprintf = require('sprintf')
+
+module.exports=mdcssParser;
+
+function mdcssParser() {
+  var jsonData=[];
+  var PATH = "../source_files";
+
+  var readSourceFiles= function() {
+    fs.readdirSync(PATH).forEach(function (sourceFile) {
+      if(/.*\.json/.test(sourceFile)) {
+        jsonData.push(JSON.parse(fs.readFileSync(PATH + "/" + sourceFile)));
+      }
+    });
+  };
+
+  this.parse= function(mdcssSrc) {
+    readSourceFiles();
+
+    var mdcssData = css.parse(mdcssSrc);
+    var cssData = { type: "stylesheet",
+                    stylesheet: {
+                      rules: []
+                    }
+                  };
+
+    mdcssData.stylesheet.rules.forEach(function(mdcssRule) {
+      winston.debug(sprintf("Rule => [%s]", JSON.stringify(mdcssRule)));
+
+      var cssRule = JSON.parse(JSON.stringify(mdcssRule));
+      cssRule.declarations = [];
+
+      mdcssRule.declarations.forEach(function(mdcssDeclaration) {
+        var valuesArray = mdcssDeclaration.value.split(" ");
+
+        var tag = getTag(mdcssDeclaration.property);        
+        var value = getValue(tag, valuesArray[0]);
+        winston.debug(sprintf("Tag [%s] Value [%s]",tag.name, value ? value.name : "NO VALUE"));
+        var variantName = valuesArray.length > 1 ? valuesArray[1] : value.defaultVariant;
+        var variant = getVariant(value, variantName);
+
+        variant.css.forEach(function(css) {
+          var cssDeclaration = {
+            type: "declaration",
+            property: css.property,
+            value: css.value
+          };
+
+          cssRule.declarations.push(cssDeclaration);
+        });        
+      });
+
+      cssData.stylesheet.rules.push(cssRule);
+    });
+
+    return css.stringify(cssData);
+  };
+
+  var getTag= function(tagName) {
+    for(var i=0; i < jsonData.length; i++) {
+      if(jsonData[i].name==tagName) {
+        return jsonData[i];
+      }
+    }
+
+    return null;
+  };
+
+  var getValue= function(tag, valueName) {
+    winston.debug(sprintf("Searching in tag [%s] for valueName [%s]", tag.name, valueName));
+    for(var i=0; i < tag.values.length; i++) {
+      if(tag.values[i].name==valueName) {
+        return tag.values[i];
+      }
+    }  
+
+    return null;
+  };
+
+  var getVariant= function(value, variantName) {
+    winston.debug(sprintf("Searching in value [%s] for variantName [%s]", value.name, variantName));
+    for(var i=0; i < value.variants.length; i++) {
+      if(value.variants[i].name==variantName) {
+        return value.variants[i];
+      }
+    }
+
+    return null;
+  };
+}
---
+++ tools/colors-generator/generator.js
@@ -0,0 +1,106 @@
+/*
+ * synonyms.js - PhantomJS script to get term definitions from Synonyms service
+ * 
+ * Receives term to search as parameter.
+ * Returns JSON string containing the result:
+ *   {
+ *     terms: [ <term>, <term>, ... ],            // list of terms returned, should be only one?
+ *     origins: [ <origin>, <origin>, ...]          // list of origins of term, should be only one?
+ *     definitions: [ <definition>, <definition>, ... ],  // list of definitions of term
+ *     expressions: [                   // list of term associated expressions 
+ *       {     
+ *         expression: <expression>,            // expression
+ *         definitions: [ <definition>, <definition>, ... ] // list of definitions of expression
+ *       }, ... 
+ *     ]
+ *   }
+ *      
+ * Result code:
+ *   0 - Term found and processed correctly
+ *   1 - Term parameter not found
+ *   2 - Timeout (5 seconds) found when accessing page.
+ *   3 - Ambiguous term found.
+ *   4 - Unknown term found.
+ *   5 - Unknown response from web site.
+ *   
+ */
+var system = require('system');
+var page = require('webpage').create();
+
+function waitFor(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                    phantom.exit(2);
+                } else {
+                    // Condition fulfilled (timeout and/or condition is 'true')
+                    // console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 250); //< repeat check every 250ms
+};
+
+var processColorPalette = function() {
+  var colorPaletteDiv = document.querySelector('.color-palette');
+  var colorGroups = colorPaletteDiv.querySelectorAll('.color-group');
+  var mdObject = { type: "tag", name: "md-color", values: [] };
+  var mdBlackColor = { name: "black", defaultVariant: 1, variants: [ { name: "1", css: [ { property: "background-color", value: "#000000" } ] } ] };
+  var mdWhiteColor = { name: "white", defaultVariant: 1, variants: [ { name: "1", css: [ { property: "background-color", value: "#ffffff" } ] } ] };
+  var mdTransparentColor = { name: "transparent", defaultVariant: 1, variants: [ { name: "1", css: [ { property: "background-color", value: "transparent" } ] } ] };
+
+  [].forEach.call(colorGroups, function(colorGroup) {
+    var ul=colorGroup.querySelector('ul');
+    var colorGroupObject = { name: "", defaultVariant: "", variants: [] };
+
+    [].forEach.call(ul.querySelectorAll('li'), function(li) {      
+      if(li.classList.contains('main-color')) {
+        var name = li.querySelector('span.name').innerText;
+        colorGroupObject.name = name.toLowerCase().replace(" ","-");
+        colorGroupObject.defaultVariant = li.querySelector('span.shade').innerText;
+      } else {
+        var name = li.querySelector('span.shade').innerText;
+        var color = li.querySelector('span.hex').innerText;
+
+        var variantObject = { css: [], name: name };
+        variantObject.css.push({ property: "background-color", value: color } );
+        var style = window.getComputedStyle(li);
+        variantObject.css.push({ property: "color", value: style.color } );
+
+        if(name != 1000) {
+          colorGroupObject.variants.push(variantObject);  
+        }        
+      }
+    });
+
+    mdObject.values.push(colorGroupObject);
+  });
+
+  mdObject.values.push(mdBlackColor);
+  mdObject.values.push(mdWhiteColor);
+  mdObject.values.push(mdTransparentColor);
+  
+  return mdObject;
+}
+
+var url = "http://www.google.com/design/spec/style/color.html";
+var child = "";
+
+page.open(url, function (status) {
+  if(status !== "success" ) {
+    console.log("ERROR!");
+  } else {
+  jsonData = page.evaluate(processColorPalette);
+  console.log(JSON.stringify(jsonData,null,2));
+  phantom.exit(0);
+  }  
+});
