a5db30f7bb9ea273a15ba28770fb7f53a5ae7f01
Dani Guardiola <contacto@daniguardiola.me>
2014-10-29 21:07:49 +0100

    - Created tools folder - Added generator of md-color.json

---
+++ grunt-materializer/lib/materializer/test/test2.js
@@ -0,0 +1,14 @@
+fs = require('fs')
+winston = require('winston')
+mdcssParser = require('../mdcssparser.js')
+
+var argv = require('optimist').argv;
+
+winston.level = 'info';
+
+if(argv.v) {
+  winston.level = 'debug';
+}
+
+var parser = new mdcssParser();
+console.log(parser.parse(".class1 { md-color: red 100; md-typo: subhead; }"));
