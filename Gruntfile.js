a3833d927e3c0a99802bdc81b1072a14b0cdf0e8
David María Arribas <dmariaa@datatrends.es>
2014-10-29 21:07:49 +0100

    - Created tools folder - Added generator of md-color.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,5 +80,4 @@ module.exports = function(grunt) {
 
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
   grunt.registerTask('default', ['compiler','concat',"autoprefixer",'cssmin','uglify']);
-
 };
