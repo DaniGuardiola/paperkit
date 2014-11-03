c4a43e9636f690ea6f549669b239c6ef334cdf5e
Dani Guardiola <contacto@daniguardiola.me>
2014-11-03 12:45:00 +0100

    - Fixed before-after

--- source/javascript/md-snackbar.js
+++ source/javascript/md-snackbar.js
@@ -1,15 +1,18 @@
 var snackanimation = function(snackbar) {
   var position = snackbar.getAttribute('md-position');
 
-  if(position.split(' ').indexOf('bottom') != -1) {
-    if(!snackbar.hasAttribute('md-notanimated')) {
-      snackbar.style.transitionProperty='bottom, opacity';
-      snackbar.style.transitionDuration="0.25s, 1s";      
-      snackbar.style.bottom="24px";
-      snackbar.style.opacity="1";
+  if(!snackbar.hasAttribute('md-notanimated')) {
+    if(position.split(' ').indexOf('bottom') != -1) {    
+        snackbar.style.transitionProperty='bottom, opacity';
+        snackbar.style.transitionDuration="0.25s, 1s";      
+        snackbar.style.bottom="24px";
+        snackbar.style.opacity="1";
+    } else {
+        snackbar.style.transitionProperty='top, opacity';
+        snackbar.style.transitionDuration="0.25s, 1s";      
+        snackbar.style.top="0px";
+        snackbar.style.opacity="1";      
     }
-  } else if(position.split(' ').indexOf('top') != -1) {
-
   }
 }
 
