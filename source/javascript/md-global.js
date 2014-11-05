f03faa9923f9a14fee03ada0e82e3f20eb00724d
David María Arribas <dmariaa@datatrends.es>
2014-11-05 09:42:30 +0100

    - Some changews not finished yet.

---
+++ source/javascript/md-global.js
@@ -0,0 +1,18 @@
+var initGlobalMDFunctions = function(MDElement) {
+  MDElement.changeProperty= function(property, oldvalue, newvalue) {
+    var attribute = this.getAttribute(property); 
+
+    if(attribute) {
+      var values = attribute.split(' ');
+      var oldvalueIndex = values.indexOf(oldvalue);
+
+      if(oldvalueIndex >= 0) {
+        values.splice(oldvalueIndex, 1, newvalue);
+      } else {
+        values.push(newvalue);
+      }
+
+      this.setAttribute(property, values.join(' '));    
+    }    
+  }
+}
