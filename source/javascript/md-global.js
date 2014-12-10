804daf916f2986612b722c16a2650a6924701e75
Dani Guardiola <contacto@daniguardiola.me>
2014-12-10 10:11:47 +0100

    - Added version 2 of compiler - Refactored md-tabbar - Added md-pager - Some small fixes

--- source/javascript/md-global.js
+++ source/javascript/md-global.js
@@ -1,5 +1,18 @@
 var initGlobalMDFunctions = function(MDElement, materializer) {
+  /* Materializer object */
   MDElement.materializer = materializer;
+  /* True if element is already initialized */
+  MDElement.alreadyInitialized = true;
+
+  /**
+   * Changes  a value of a property, supporting multiple value properties;
+   * If oldvalue is null, it adds the new value. If oldvalue is set, then
+   * it's replaced by new value.
+   * 
+   * @param  {string} property Property name.
+   * @param  {string} oldvalue Old value to replace.
+   * @param  {string} newvalue New value to set.
+   */
   MDElement.changeProperty= function(property, oldvalue, newvalue) {
     var attribute = this.getAttribute(property); 
 
@@ -9,7 +22,9 @@ var initGlobalMDFunctions = function(MDElement, materializer) {
 
       if(oldvalueIndex >= 0) {
         values.splice(oldvalueIndex, 1, newvalue);
-      } else {
+      }
+
+      if(newvalue) {
         values.push(newvalue);
       }
 
@@ -17,6 +32,10 @@ var initGlobalMDFunctions = function(MDElement, materializer) {
     }    
   }
 
+  /**
+   * Generates aleatory unique id
+   * @return {string} Aleatory unique id
+   */
   MDElement.makeId= function()
   {
       var text = "";
@@ -28,5 +47,14 @@ var initGlobalMDFunctions = function(MDElement, materializer) {
       return text;
   }
 
-  MDElement.alreadyInitialized = true;
+  /**
+   * Calls a user function, setting window as context.
+   * 
+   * @param  {string} f      function name
+   * @param  {array}  target parameters pased to function. in this case the target of the event.
+   */
+  MDElement.callUserFunction= function(functionName, params) {
+    console.log("Calling user function " + functionName);
+    executeFunctionByName(functionName, window, params);
+  }
 }
