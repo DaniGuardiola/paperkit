94ff8deeea6242e0917bbb01788a996bae6e0493
Dani Guardiola <dani.guardiola@datatrends.es>
2014-12-30 12:07:28 +0100

    -Added bower versions (generated on Gruntfile.js) -Added flex and shrink generator, and icon fallback experiment -Minor corrections

---
+++ md-elements-demo/script.js
@@ -0,0 +1,2 @@
+var md = new Materializer(); // Instantiate Materializer on a variable of your choice
+window.addEventListener('load', function(){ md.init() }); // Initialize Materializer on window load
