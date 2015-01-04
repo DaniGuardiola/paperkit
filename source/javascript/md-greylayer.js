23511efdb5e8dfd32670a219e05c4a7213840f2f
Dani Guardiola <contacto@daniguardiola.me>
2015-01-04 19:04:42 +0100

    - Added some documentation - Renamed switch function to toggle to avoid problems

--- source/javascript/md-greylayer.js
+++ source/javascript/md-greylayer.js
@@ -1,16 +1,25 @@
 var initMDGreylayer = function(MDGreylayer) {
+  /**
+   * Shows the grey layer.
+   */
   MDGreylayer.show = function() {
     MDGreylayer.removeEventListener(transitionend, MDGreylayer.noZIndex);
     MDGreylayer.style.zIndex = "400";
     MDGreylayer.setAttribute("md-state", "on");
   }
 
+  /**
+   * Hides the grey layer.
+   */
   MDGreylayer.hide = function() {
     MDGreylayer.setAttribute("md-state", "off");
     MDGreylayer.addEventListener(transitionend, MDGreylayer.noZIndex);
   }
 
-  MDGreylayer.switch = function() {
+  /**
+   * Toggles grey layer state, showing or hiding it.
+   */
+  MDGreylayer.toggle = function() {
     if (MDGreylayer.getAttribute('md-state') !== "on") {
       MDGreylayer.show();
     } else {
@@ -18,10 +27,20 @@ var initMDGreylayer = function(MDGreylayer) {
     }
   }
 
+  /** 
+   * Removes z-index from grey layer.
+   * TODO: REVIEW
+   */
   MDGreylayer.noZIndex = function() {
     MDGreylayer.style.zIndex = "";
   }
  
+  /**
+   * Callback function, called when an attribute changes.
+   * @param {string} attrname Changed attribute name
+   * @param {string} oldvalue Old value for attribute or null if previous value does not exist.
+   * @param {string} newvalue New value for attribute or null if value removed.
+   */
   MDGreylayer.attributeChangedCallback = function(attrname, oldvalue, newvalue) {
     console.log("CHANGED ATTRIBUTE " + attrname + " VALUE " + newvalue);
   };
