2ca5dbb19dd3dc3a7706191c28e6ccafb91ebf7a
DaniGuardiola <contacto@daniguardiola.me>
2015-01-20 11:38:30 +0100

    - Project of md-tile

---
+++ source/javascript/md-tile.js
@@ -0,0 +1,17 @@
+var initMDTile = function(MDTile,materializer) {
+  var createAvatarTile= function(text, avatarImage, iconImage, handler){
+    
+  }
+  
+  var createSimpleTile= function(text, handler) {
+    
+  }
+  
+  var createIconTile= function(text, iconImage, handler) {
+    
+  }
+  
+  var createIconButtonTile= function(text, iconButtonImage, handler) {
+    
+  }
+}
