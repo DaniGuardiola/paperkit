66e5907d0fd7f622b55138c7c095734ab1134ba3
CarlosJeurissen <ccbjeurissen@gmail.com>
2015-03-17 19:57:02 +0100

    modify the structure of the log module

--- src/core/js/log.js
+++ src/core/js/log.js
@@ -1,13 +1,17 @@
+/* global md */
 (function() {
   "use strict";
-  if (window.md === undefined) {
-    console.error("[Paperkit] [log] Paperkit was not found!");
-  } else {
-    return {
-      "someTest": test,
-      "someOtherTest": anotherTest,
-      "againSomeOtherTest": againAnotherTest,
-      "a": aVariable
-    };
+  var moduleName = "log";
+
+  if(window.md === undefined) {
+    console.error("[Paperkit] [" + moduleName + "] Paperkit was not found!");
+    return;
   }
+
+  //start initializing the module, defining functions and variables and such.
+
+
+
+  //add the components with it's variables and functions to the md namespace
+  md.log = {};
 })();
