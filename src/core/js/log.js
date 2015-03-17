9a683c45d66cf4f8668e07adc85cc17593c990b5
DaniGuardiola <contacto@daniguardiola.me>
2015-03-17 19:50:35 +0100

    New log

--- src/core/js/log.js
+++ src/core/js/log.js
@@ -1,23 +1,7 @@
 (function() {
   "use strict";
-  var aVariable = "awesome";
-
-  function test() {
-    //something
-  }
-
-  function anotherTest() {
-    //something
-  }
-
-  function againAnotherTest() {
-    //something
-  }
-
-
-
   if (window.md === undefined) {
-    console.error("Please load core before something");
+    console.error("[Paperkit] [log] Paperkit was not found!");
   } else {
     return {
       "someTest": test,
