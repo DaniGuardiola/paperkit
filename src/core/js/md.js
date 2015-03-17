7cb85ae592a6f723bbbe1b20a846d060b5afb43b
DaniGuardiola <contacto@daniguardiola.me>
2015-03-17 19:47:59 +0100

    Yup I just uploaded a not-linted code. Here's the fix.

--- src/core/js/md.js
+++ src/core/js/md.js
@@ -3,6 +3,7 @@
   if (window === undefined) {
     console.error("[Paperkit] Window is undefined. What are you doing? It is the weirdest thing that ever happened to a web framework! D: I'm scared");
   } else {
-	window.md = {};
-	console.log(global.md);
+    window.md = {};
+    console.log(window.md);
+  }
 }());
